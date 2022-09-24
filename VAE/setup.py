bdf24ccb7ec62f0abaee5b2f83383474fa3a6867
Shashank Kumar <shashank14k@gmail.com>
2022-09-24 14:32:16 +0530

    Added setup.py

---
+++ VAE/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vae_generator',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/shashank14k/Generative-Models/tree/main/VAE',
+    license='',
+    author='Shashank',
+    author_email='shashank14k@gmail.com',
+    description='Class to build VAE model from a list of model parameters and train it'
+)
